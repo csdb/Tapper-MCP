b03e33dcd3079a03b124aff69e0f6334e459792c
Maik Hentsche <maik.hentsche@amd.com>
2009-06-02 15:04:34 +0200

    MCP: added interface classes for scheduler

---
+++ lib/Artemis/MCP/Scheduler/TestRequest.pm
@@ -0,0 +1,46 @@
+use MooseX::Declare;
+
+    
+class Artemis::MCP::Scheduler::TestRequest {
+
+=head1 NAME
+        
+   Artemis::MCP::Scheduler::TestRequest - Object that handles requesting new tests
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+=head1 SYNOPSIS
+
+
+
+=head1 FUNCTIONS
+
+=cut
+        
+
+{
+        # just for CPAN
+        package Artemis::MCP::Scheduler::Builder;
+        our $VERSION = '0.01';
+}
+
+
+=head1 AUTHOR
+
+Maik Hentsche, C<< <maik.hentsche at amd.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Maik Hentsche, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of WFQ
