b390c545a5006f0f11d77217dc895c96322238f9
Maik Hentsche <maik.hentsche@amd.com>
2009-06-08 10:31:13 +0200

    MCP: WFQ does no longer need a hostname since it only needed it to call the producer which it does not do any more

--- lib/Artemis/MCP/Scheduler/Algorithm/WFQ.pm
+++ lib/Artemis/MCP/Scheduler/Algorithm/WFQ.pm
@@ -48,13 +48,11 @@ Return the virtual finishing time of a given client
 
 Evaluate which client has to be scheduled next.
 
-@param string - get a testrun for this host
-
 @return success - client name;
 
 =cut
 
-        method get_next_job(Str $hostname)
+        method get_next_job()
         {
                 my $vft;
                 my $queue;
