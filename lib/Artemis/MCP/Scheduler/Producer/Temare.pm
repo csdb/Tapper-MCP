866787385b81eac52444d1942a81ebda629d7470
Maik Hentsche <maik.hentsche@amd.com>
2009-06-15 09:00:04 +0200

    MCP: added Producer wrapper for temare

---
+++ lib/Artemis/MCP/Scheduler/Producer/Temare.pm
@@ -0,0 +1,47 @@
+use MooseX::Declare;
+
+    
+class Artemis::MCP::Scheduler::Producer::Temare extends Producer {
+
+=head1 NAME
+        
+   Artemis::MCP::Scheduler::Producer::Temare - Wraps the existing temare producer
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+=head1 SYNOPSIS
+
+
+=cut
+
+=head2 features
+
+=cut 
+
+}
+{
+        # just for CPAN
+        package Artemis::MCP::Scheduler::Producer::Temare;
+        our $VERSION = '0.01';
+}
+
+
+=head1 AUTHOR
+
+Maik Hentsche, C<< <maik.hentsche at amd.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Maik Hentsche, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1;
