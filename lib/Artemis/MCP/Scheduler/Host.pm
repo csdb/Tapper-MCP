58297f2677584e0ead9927261cc08cfb8006cf05
Steffen Schwigon <steffen.schwigon@amd.com>
2009-08-13 11:15:59 +0200

    Scheduler: synopsis + reformatting

--- lib/Artemis/MCP/Scheduler/Host.pm
+++ lib/Artemis/MCP/Scheduler/Host.pm
@@ -1,17 +1,15 @@
 use MooseX::Declare;
 
-    
+
 class Artemis::MCP::Scheduler::Host {
 
 =head1 NAME
-        
-  Host - Implements a host object used for scheduling
+
+Host - Implements a host object used for scheduling
 
 =head1 VERSION
 
 Version 0.01
-
-=cut
 
 =head2 name
 
@@ -23,7 +21,8 @@ Hostname. Has to be unique.
 
 =head2 state
 
-A host can have a certain state. Since it is not clear yet how to use this attribute possible values are also unknown yet.
+A host can have a certain state. Since it is not clear yet how to use
+this attribute possible values are also unknown yet.
 
 =cut
 
@@ -31,7 +30,8 @@ A host can have a certain state. Since it is not clear yet how to use this attri
 
 =head2 features
 
-List of features offered by a host. Can be used to decide whether a certain host fits the requirement list of a test request.
+List of features offered by a host. Can be used to decide whether a
+certain host fits the requirement list of a test request.
 
 =cut
 
@@ -41,7 +41,26 @@ List of features offered by a host. Can be used to decide whether a certain host
 
 =head1 SYNOPSIS
 
-
+ my $host = Artemis::MCP::Scheduler::Host->new
+    (
+     name               =>'bullock',
+     state              => 'free',
+     available_features => {
+                            Mem             => 8192,
+                            Vendor          => 'AMD',
+                            Family          => 15,
+                            Model           => 67,
+                            Stepping        => 2,
+                            Revision        => '',
+                            Socket          => 'AM2',
+                            Number_of_cores => 2,
+                            Clock           => 2600,
+                            L2_Cache        => 1024,
+                            L3_Cache        => 0
+                           },
+    );
+ push @hostlist, $host;
+ my $job = $primat->get_next_job(\@hostlist);
 
 =head1 FUNCTIONS
 
@@ -51,7 +70,7 @@ List of features offered by a host. Can be used to decide whether a certain host
 
 {
     # just for CPAN
-    package Host;
+    package Artemis::MCP::Scheduler::Host;
     our $VERSION = '0.01';
 }
 
@@ -62,9 +81,11 @@ Maik Hentsche, C<< <maik.hentsche at amd.com> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-wfq at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WFQ>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests to C<bug-wfq at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WFQ>.  I will be
+notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
 
 
 =head1 COPYRIGHT & LICENSE
