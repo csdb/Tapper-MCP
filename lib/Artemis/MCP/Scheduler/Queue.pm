1ce58cf9faf90714e561e0dc69a62da79c9f16d0
Steffen Schwigon <steffen.schwigon@amd.com>
2009-09-01 16:42:49 +0200

    Scheduler: Return of the Scheduler::Queue. This time it extends Result::Queue.

---
+++ lib/Artemis/MCP/Scheduler/Queue.pm
@@ -0,0 +1,33 @@
+use MooseX::Declare;
+
+class Artemis::MCP::Scheduler::Queue extends Artemis::Schema::TestrunDB::Result::Queue
+{
+
+        sub producer
+        {
+                my ($self) = @_;
+
+                my $producer_class = "Artemis::MCP::Scheduler::PreconditionProducer::".$self->producer;
+                eval "use $producer_class";
+                return $producer_class->new unless $@;
+                return undef;
+        }
+
+        sub produce
+        {
+                my ($self, $request) = @_; # TestRequest
+
+                if (not $self->producer) {
+                        warn "Queue ".$self->name." does not have an associated producer";
+                } else {
+                        print STDERR "Queue.produce/producer: ", Dumper($self->producer);
+                        return $self->producer->produce($request)
+                }
+        }
+}
+
+{
+        # just for CPAN
+        package Artemis::MCP::Scheduler::Queue;
+        our $VERSION = '0.01';
+}
