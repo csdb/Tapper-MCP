e5a4bdf963967103bfa05eb8ed290026d0d428d3
Steffen Schwigon <steffen.schwigon@amd.com>
2009-09-04 13:56:08 +0200

    Scheduler: TypeLib tests

--- xt/MUser.pm
+++ xt/MUser.pm
@@ -2,7 +2,8 @@
 # -------------------- this does NOT work --------------------
 
 use MooseX::Declare;
-class MUser extends DbUser {
+class MUser {
+        extends 'DbUser';
         extends 'Class::Accessor::Fast'; # NEEDED!?
         has hotstuff => (is => 'rw', isa => "Str");
         __PACKAGE__->meta->make_immutable(inline_constructor => 0);
