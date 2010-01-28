5bbe488c88c8502018a034c880bce2e49e7cfd69
Maik Hentsche <maik.hentsche@amd.com>
2010-01-28 15:29:36 +0100

    MCP: added producer for newest package in given source dir

---
+++ lib/Artemis/MCP/Scheduler/PreconditionProducer/NewestPackage.pm
@@ -0,0 +1,69 @@
+use MooseX::Declare;
+
+class Artemis::MCP::Scheduler::PreconditionProducer::NewestPackage extends Artemis::MCP::Scheduler::PreconditionProducer
+{
+        use YAML;
+
+        use 5.010;
+
+        use aliased 'Artemis::Config';
+        use File::stat;
+
+        sub younger { stat($a)->mtime() <=> stat($b)->mtime() }
+
+        method produce(Any $job, HashRef $produce) {
+
+                my $source_dir    = $produce->{source_dir};
+                my @files = sort younger <$source_dir/*>;
+                return {
+                        error => 'No files found in $source_dir',
+                       } if not @files;
+                my $use_file = pop @files;
+
+                my $nfs = Config->subconfig->{paths}{prc_nfs_mountdir};
+                return {
+                        error => "$use_file not available to Installer",
+                       } unless $use_file=~/^$nfs/;
+
+                $retval = [
+                           {
+                            precondition_type => 'package',
+                            filename => $use_file;
+                           }
+                          ];
+                return {
+                        precondition_yaml => Dump(@$retval),
+                       };
+        }
+        
+        
+
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Artemis::MCP::Scheduler::PreconditionProducer::NewestPackage - Produces a
+package precondition that installs the newest package from a given directory.
+
+=head1 SYNOPSIS
+
+
+=cut
+
+=head2 features
+
+=head1 AUTHOR
+
+Maik Hentsche, C<< <maik.hentsche at amd.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Maik Hentsche, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
