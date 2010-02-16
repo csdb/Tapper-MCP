9401c22d64006af79032842af1031fe270a913e9
Maik Hentsche <maik.hentsche@amd.com>
2010-02-16 20:11:50 +0100

    MCP: [FEAT] Remove MergedQueue from scheduler controller
    Controller is the base modul of scheduler. The mergedqueue is no longer
    required and I removed it. Now the queues themselves are responsible for
    fitting hosts. Additionally we need a priority queue to make sure tests
    belonging to the same scenario are scheduled close together. This patch brings
    this priority queue.

--- lib/Artemis/MCP/Scheduler/Controller.pm
+++ lib/Artemis/MCP/Scheduler/Controller.pm
@@ -6,11 +6,11 @@ class Artemis::MCP::Scheduler::Controller
 {
         use Perl6::Junction qw/ any /;
         use Artemis::Model 'model';
-        use Artemis::MCP::Net;
         use aliased 'Artemis::MCP::Scheduler::Algorithm';
-        use aliased 'Artemis::MCP::Scheduler::MergedQueue';
+        use aliased 'Artemis::MCP::Scheduler::PrioQueue';
 
         has hostlist  => (is => 'rw', isa => 'ArrayRef');
+        has prioqueue => (is => 'rw', isa => PrioQueue, default => sub { PrioQueue->new });
         has algorithm => (is => 'rw',
                           isa => 'Artemis::MCP::Scheduler::Algorithm',
                           default => sub {
@@ -20,89 +20,6 @@ class Artemis::MCP::Scheduler::Controller
                                           );
                                          }
                          );
-        has merged_queue => (is => 'rw', isa => MergedQueue, default => sub { MergedQueue->new });
-
-        method BUILD {
-                $self->merged_queue->wanted_length( $self->algorithm->queue_count );
-        }
-
-        method fill_merged_queue()
-        {
-                my $count_missing_jobs = $self->merged_queue->wanted_length - $self->merged_queue->length;
-                my %queues;
-                my $queue_rs = model('TestrunDB')->resultset('Queue');
-                foreach my $queue_name( map {$_->name} $queue_rs->all ) {
-                        $queues{$queue_name} = 1;
-                }
-
-                # fill up to wanted merged_queue length, only accept shorter merged_queue if no queue has jobs to offer
-                while($count_missing_jobs > 0)
-                {
-                        my $queue;
-                        do {
-                                $queue = $self->algorithm->get_next_queue();
-                        } while ($queue and not $queue->active);
-                        my $testrun_rs = $queue->queued_testruns;
-                        my $job   = $testrun_rs->first;
-                        if ($job) {
-                                $count_missing_jobs--;
-                                $self->merged_queue->add($job);
-                        } else {
-                                delete $queues{$queue->name};
-                        }
-                        last if not %queues;
-                }
-        }
-
-        method adapt_merged_queue_length(Any $job, ArrayRef $free_hosts) {
-                if (not $job) {
-                        # increase merged_queue length if no job found,
-                        if ($self->merged_queue->wanted_length - $self->merged_queue->length < 1) # not longer than current length + 1
-                        {
-                                my $lookup_queue = $self->algorithm->lookup_next_queue();
-                                my $queuehosts   = $lookup_queue->queuehosts;
-
-                                if ( grep {$_->{host}->queuehosts->count == 0 } @$free_hosts   ) { # increase if at least one host is free for all queues
-                                        $self->merged_queue->wanted_length( $self->merged_queue->wanted_length + 1 );
-
-                                } elsif ($queuehosts->count) {                                     # increase if at least one host is bound to next queue
-                                        my @queuehosts = map {$_->host->name}         $queuehosts->all;
-                                        my @free_hosts = map {$_->{host}->name} @$free_hosts;
-                                        if (any(@queuehosts) eq any(@free_hosts)) {
-                                                $self->merged_queue->wanted_length( $self->merged_queue->wanted_length + 1 );
-                                        }
-                                } else {
-                                        $self->algorithm->get_next_queue;
-                                }
-                        }
-                } else {
-                        # count down merged_queue again on success,
-                        # but not smaller that count queues
-                        $self->merged_queue->wanted_length( $self->merged_queue->wanted_length - 1 )
-                            if $self->merged_queue->wanted_length > $self->algorithm->queue_count;
-                }
-        }
-
-        method overfill_merged_queue()
-        {
-                my $queue_rs = model('TestrunDB')->resultset('Queue');
-                my %queues;
-                foreach my $queue( $queue_rs->all ) {
-                        $queues{$queue->name} = $queue;
-                }
-
-                my $jobs = $self->merged_queue->get_testrequests;
-                foreach my $tr( $jobs->all() ) {
-                        delete($queues{$tr->queue->name}) if defined $queues{$tr->queue->name};
-                }
-
-                foreach my $queue_name (keys %queues) {
-                        my $queue      = $queues{$queue_name};
-                        my $testrun_rs = $queue->queued_testruns;
-                        my $job        = $testrun_rs->first;
-                        $self->merged_queue->add($job) if $job;
-                }
-        }
 
 
         method get_next_job(Any %args) {
@@ -110,16 +27,44 @@ class Artemis::MCP::Scheduler::Controller
 
                 do {{
 
-                        $self->fill_merged_queue;
                         my $free_hosts = Artemis::Model::free_hosts_with_features();
                         return if not ($free_hosts and @$free_hosts);
-                        $job = $self->merged_queue->get_first_fitting($free_hosts);
-                        $self->overfill_merged_queue() if not $job;
+                        
+                        my %queues;
+                        my $queue_rs = model('TestrunDB')->resultset('Queue');
+                        %queues = map {$_->name, 1} $queue_rs->all;
 
+                QUEUE:
+                        while (1) {
+                                # ask prioqueue everytime when in loop because new priority jobs
+                                # that got into DB between to loop runs still have highest priority
+                                last QUEUE if $job = $self->prioqueue->get_first_fitting($free_hosts);
+                                
+                                
+                                my $queue = $self->algorithm->get_next_queue();
+                                if ($job = $queue->get_first_fitting($free_hosts)) {
+                                        if ($job->auto_rerun) {
+                                                $job->testrun->rerun;
+                                        }
+                                        if ($job->testrun->scenario_element) {
+                                        ELEMENT:
+                                                foreach my $element ($job->testrun->scenario_element->peer_elements) {
+                                                        my $peer_job = $element->testrun->testrun_scheduling;
+                                                        next ELEMENT if $peer_job->id == $job->id;
+                                                        $self->prioqueue->add($peer_job);
+                                                }
+                                        }
+                                        last QUEUE;
+                                } else {
+                                        delete $queues{$queue->name};
+                                }
+                                last QUEUE if not %queues;
+                        }
+                        
                         my $error;
                         eval{
-                                $error=$job->produce_preconditions() if $job;
-                        };
+                                 $error=$job->produce_preconditions() if $job;
+                         };
                         if ($error or $@) {
                                 $error //=$@;
                                 my $net    = Artemis::MCP::Net->new();
