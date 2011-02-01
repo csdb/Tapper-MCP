6bd2a97c7c4cd1e0ffd471ee33ac78a6614ad6a2
Maik Hentsche <maik.hentsche@amd.com>
2011-02-01 13:11:00 +0100

    MCP: [FIX] timeout handling for scheduler
    Scheduler is supposed to be called every 10 seconds. Due to a bug in
    timeout calculation it was only called when there was no console data
    for 10 seconds. This bug is fixed now.

--- lib/Artemis/MCP/Master.pm
+++ lib/Artemis/MCP/Master.pm
@@ -345,7 +345,8 @@ itself is put outside of function to allow testing.
         {
                 my ($self, $lastrun) = @_;
                 my $timeout          = $lastrun + $self->cfg->{times}{poll_intervall} - time();
-
+                $timeout = 0 if $timeout < 0;
+                
                 my @ready;
                 # if readset is empty, can_read immediately returns with an empty
                 # array; this makes runloop a CPU burn loop
@@ -370,7 +371,7 @@ itself is put outside of function to allow testing.
                         }
                 }
 
-                if (not @ready) {
+                if (($timeout <= 0) or (not @ready)) {
                         while ( my @jobs = $self->scheduler->get_next_job() ) {
                                 foreach my $job (@jobs) {
                                         # (WORKAROUND) try to avoid to
@@ -380,8 +381,10 @@ itself is put outside of function to allow testing.
                                         sleep 2;
                                         $self->run_due_tests($job);
                                 }
+                                $lastrun = time();
                         }
                 }
+                return $lastrun;
         }
 
 
@@ -437,9 +440,9 @@ Set up all needed data structures then wait for new tests.
                 $self->set_interrupt_handlers();
                 $self->prepare_server();
                 $self->revive_children();
+                my $lastrun = time();
                 while (1) {
-                        my $lastrun = time();
-                        $self->runloop($lastrun);
+                         $lastrun = $self->runloop($lastrun);
                 }
 
         }
